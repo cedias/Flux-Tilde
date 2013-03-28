fffea8237acb114cc41361ed9827c2a7431d2b91
Charles-Emmanuel Dias <charles-emmanuel.dias@etu.upmc.fr>
2013-03-28 17:00:33 +0100

    list working

--- app/js/MusicLinks.js
+++ app/js/MusicLinks.js
@@ -1,14 +1,91 @@
 
+function MusicLinks (args) {
 
+	//options
+	this.soundcloudId = args.soundcloudId;
+	this.firebase = new Firebase(args.firebase);
+	this.binds = args.binds;
+	this.debug = args.debug;
 
-function MusicLinks (firebase,client_id) {
-
-	var that = this;
-	this.client_id = client_id;
-	this.firebase = new Firebase(firebase);
+	//attributes
 	this.linkList = [];
+	this.track = 0;
 
-	SC.initialize({client_id:client_id});
+	this._init();
+
+}
+
+MusicLinks.prototype._init = function(){
+
+	//init soundcloud api
+	SC.initialize({client_id:this.soundcloudId});
+
+	//binds firebase list to app
+	this._bindList();
+
+	//bind controls
+	this._bindControls();
+
+};
+
+MusicLinks.prototype._bindList = function() {
+	var that = this;
+	var b = this.binds;
+	var template = $(b.template).html();
+
+	//on dataChange
+	this.firebase.on('value', function(snapshot) {
+
+		//reseting linklist
+		that.linkList = [];
+
+		//iterating over links
+		snapshot.forEach(function(child){
+
+			var msgData = child.val();
+	  		var temp = Handlebars.compile(template);
+
+	  		//saving each track
+			that.linkList.push(child.val());
+			//adding element to list
+	  		$(b.elementList).append(temp(msgData));
+
+		});
+
+
+		//unbind previous binds
+		$(b.elementList+' li').off('click');
+
+		//binding clickEvent on music list
+	  	var items = $(b.elementList+' li').on('click',function() {
+			    var index = items.index(this);
+			    that.play(index);
+
+			    if(that.debug){
+			    	console.log("Clicked on: " +index);
+			    }
+		});
+
+	});
+};
+
+MusicLinks.prototype._bindControls = function() {
+	var b = this.binds;
+	var that = this;
+
+	$(b.post).on('click', function(){
+		var url = $(b.link).val();
+		that.post(url);
+	});
+
+	$(b.play).on('click', function(){
+		that.play(0);
+	})
+
+	$("#pause").on('click', function(){
+		that.pause();
+	})
+
 
 }
 
@@ -27,37 +104,48 @@ MusicLinks.prototype.post = function(url) {
 	}
 };
 
-MusicLinks.prototype.bindList = function(listId, template) {
-	var that = this;
-
-	this.firebase.on('child_added', function(snapshot) {
-		var msgData = snapshot.val();
-  		var temp = Handlebars.compile(template);
-
-		that.linkList.push(snapshot.val());
-  		$(listId).append(temp(msgData));
-	});
-
-};
-
-
 MusicLinks.prototype.play = function(trackNumber){
+
+	if(this.musicPlayer !== undefined)
+		this.stop();
+
+	if(trackNumber === undefined)
+		var trackNumber = this.track;
+	else
+		this.track = trackNumber;
+
+	if(this.debug){
+		console.log('playing ' +this.track)
+		console.log(this.linkList);
+	}
 	var that = this;
 	var trackUrl = "/tracks/" + this.linkList[trackNumber].info.id;
-	var nextTrack = (trackNumber++)%this.linkList.length;
 
-	if(trackNumber === undefined && this.musicPlayer !== undefined) {
-		this.musicPlayer.play();
-	}
-		else
-	{
-		SC.stream(trackUrl, function(musicPlayer){
 
-			that.musicPlayer = musicPlayer;
-	  		musicPlayer.play();
-	  		//musicPlayer.onfinish(that.play(nextTrack)) infinite loop
-		});
+	SC.stream(trackUrl,function(musicPlayer){
+		that.musicPlayer = musicPlayer;
+  		that.musicPlayer.play();
+	});
+	
+}
+
+MusicLinks.prototype.next = function(){
+	this.track = ((this.track + 1)%this.linkList.length);
+
+	if(this.musicPlayer !== undefined)
+		this.play();
+}
+
+MusicLinks.prototype.previous = function(){
+
+	this.track -= 1;
+	
+	if(this.track < 0){
+		this.track = this.linkList.length-1;
 	}
+
+	if(this.musicPlayer !== undefined)
+		this.play();
 }
 
 MusicLinks.prototype.stop = function(){
@@ -69,5 +157,6 @@ MusicLinks.prototype.stop = function(){
 MusicLinks.prototype.pause = function(){
 
 	if(this.musicPlayer !== undefined)
-		this.musicPlayer.pause();
+		this.musicPlayer.togglePause();
+
 }
--- app/js/app.js
+++ app/js/app.js
@@ -1,30 +1,22 @@
 
 
-var ml = new MusicLinks('music-links.firebaseIO.com','3f45abb44a7e483b35d0b04047849967');
+var options = {
+	firebase:'music-links.firebaseIO.com',
+	soundcloudId:'3f45abb44a7e483b35d0b04047849967',
+	binds: {
+		post:'#linkForm',
+		link:'#link',
+		play:'#play',
+		pause:'#pause',
+		next:'#next',
+		previous:'#previous',
+		elementList:'#linkList',
+		template:'#element-template'
+	},
+	debug:true
+}
 
-$("#linkForm").on('click', function(){
-	var url = $("#link").val();
-	ml.post(url);
-});
-
-$("#play").on('click', function(){
-	ml.play(0);
-})
-
-$("#pause").on('click', function(){
-	ml.pause();
-})
-
-
-var source   = $("#element-template").html();
-ml.bindList(linkList, source);
-
+var ml = new MusicLinks(options);
 
 
 
-
-
-/*
-SC.stream("/tracks/293", function(sound){
-  sound.play();
-});*/
